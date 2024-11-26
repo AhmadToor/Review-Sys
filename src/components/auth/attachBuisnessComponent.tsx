f5cbeac60162d3a02c14b6a3bdd2581f7e31134e
Ahmad Toor <ahmadtoor4321@gmail.com>
2024-11-26 15:30:21 +0500

    Added Navigation

--- src/components/auth/attachBuisnessComponent.tsx
+++ src/components/auth/attachBuisnessComponent.tsx
@@ -1,8 +1,10 @@
 import PageIcon from '@/assets/svg/AttachBuisnessPageIcon.svg?react'
 import GoogleIcon from '@/assets/svg/GoogleIcon.svg?react'
 import { Button } from '../ui/button';
+import { useNavigate } from 'react-router-dom';
 
 const AttachBuisnessComponent = () => {
+  const navigate = useNavigate()
   return (
     <div className='flex flex-col pb-6 justify-between  items-center'>
       <div className='h-full items-center flex'><PageIcon /></div>
@@ -10,7 +12,7 @@ const AttachBuisnessComponent = () => {
       <h1 className='text-2xl md:text-3xl text-center font-bold  bg-secondary-gradient bg-clip-text text-fill'>Please connect your  Google Business Profile.</h1>
       <p className='text-gray-500 mt-1 text-base text-center'>To connect, you will need to be the owner or admin. This allows us to monitor your reviews.</p>
       <Button className='w-full mt-6'><GoogleIcon/> Link Google Business Account</Button>
-      <Button variant='outline' className='w-full mt-5 mb-2'>I'll do it later</Button>
+      <Button variant='outline' className='w-full mt-5 mb-2' onClick={()=>{navigate('/')}}>I'll do it later</Button>
       </div>
     </div>
   )
