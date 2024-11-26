7a0494df6a273cc4fcbc22f7c80fdaa7e2d4638a
Ahmad Toor <ahmadtoor4321@gmail.com>
2024-11-26 18:01:32 +0500

    Added BuisnessProfile Logic

--- src/hooks/useSignIn.tsx
+++ src/hooks/useSignIn.tsx
@@ -3,15 +3,20 @@ import { SubmitHandler, useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { signinschema } from "@/schema/authSchema"
 import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from "react-router-dom";
 import { loginRequest } from "@/services/authServices";
+import { BuisnessProfileContext } from "@/App";
+import { useContext } from "react";
 
 
 
 export const useSignIn = ()=>{
+    const {setBuisnessProfile}= useContext(BuisnessProfileContext)
     const { register, handleSubmit, formState } = useForm<SigninSchemaTypes>({
         resolver: zodResolver(signinschema),
         mode: "onSubmit"
     })
+    const navigate = useNavigate()
     const mutation = useMutation({
         mutationKey : ['sign-in'],
         mutationFn: async (values: SigninSchemaTypes) => {
@@ -25,9 +30,10 @@ export const useSignIn = ()=>{
     onSuccess : (data)=>{
         const {token , attachedBuisness, ...rest} = data
         localStorage.setItem('accessToken' , JSON.stringify(token))
-        localStorage.setItem('attachedBuisness' , JSON.stringify(attachedBuisness))
+        localStorage.setItem('attachedBuisness' , attachedBuisness)
         localStorage.setItem('user' , JSON.stringify(rest))
-        window.location.href = '/'
+        setBuisnessProfile(attachedBuisness)
+        navigate('/')
     },
     onError : (err)=>{
         return err
