32814f3879e6351aa7f73740237a8014686739be
Ahmad Toor <ahmadtoor4321@gmail.com>
2024-11-29 16:04:43 +0500

    Implemented Review on Response Page

--- src/components/emailresponse/emailresponseComponent.tsx
+++ src/components/emailresponse/emailresponseComponent.tsx
@@ -1,13 +1,18 @@
-import { Angry, ArrowLeft, Mail, Star } from "lucide-react";
+import { Angry, ArrowLeft, Mail, Smile, Star } from "lucide-react";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
 import EmailTextEditor from "./emailTextEditor";
 import { Badge } from "../ui/badge";
 import { Avatar, AvatarImage } from "../ui/avatar";
+import { useNavigate, useParams } from "react-router-dom";
+import { reviews } from "@/data/unRepliedReviews";
 
 
 
 const EmailResposeComponent = () => {
+    const {reviewId} = useParams()
+    const selectedReview = reviews.find(review => review.id === reviewId  )
+    const navigate = useNavigate()
   return (
     <div className="grid pt-6 grid-cols-[6fr_5fr] lg:grid-cols-[3fr_2fr] gap-3">
       <Card className="border-none rounded-xl  relative">
@@ -28,7 +33,7 @@ const EmailResposeComponent = () => {
         <CardFooter className="absolute flex-col bottom-3 p-0 right-3 left-3">
             <hr className="w-full my-2"/>
             <div className="flex justify-between w-full ">
-                <Button variant='outline' className="border-none bg-dashboardButton fill-primary stroke-primary hover:text-black hover:fill-black text-xs text-primary">
+                <Button variant='outline' onClick={()=>{navigate('/dashboard')}} className="border-none bg-dashboardButton fill-primary stroke-primary hover:text-black hover:fill-black text-xs text-primary">
                   <ArrowLeft/> Go Back
                 </Button>
                 
@@ -50,20 +55,20 @@ const EmailResposeComponent = () => {
             <div className="justify-between flex flex-row items-center">
         <div className="flex gap-2 items-center">
                     <Avatar className="h-8 w-8">
-                        <AvatarImage src='https://github.com/shadcn.png' alt="Avatar" className="h-8 w-8 rounded-full" />
+                        <AvatarImage src={selectedReview?.avatar} alt="Avatar" className="h-8 w-8 rounded-full" />
                     </Avatar>
     
                     <div >
-                        <h2 className="text-sm font-semibold">Maoshe Found</h2>
+                        <h2 className="text-sm font-semibold">{selectedReview?.fullname}</h2>
                         <p className="text-gray-400 text-[8px]  ">Posted 1 hour ago.</p>
                     </div>
                 </div>
                 <div className="flex gap-2">
-                <Badge className="w-fit text-xs flex items-center " ><Angry className="w-4 h-4 stroke-1.5 fill-red-400" /></Badge>
-                <Badge className="w-fit text-xs flex items-center gap-1" > <Star className="stroke-none fill-yellow-400 h-4 w-4"/>  1.5</Badge>
+                <Badge className="w-fit text-xs flex items-center " >{selectedReview?.mood === 'Happy' ? <Smile className="fill-yellow-400 stroke-1.5 w-4 h-4" /> : <Angry className="fill-red-400 stroke-1.5 w-4 h-4 stroke-black" />}</Badge>
+                <Badge className="w-fit text-xs flex items-center gap-1" > <Star className="stroke-none fill-yellow-400 h-4 w-4"/>  {selectedReview?.rating}</Badge>
                 </div>
         </div>
-        <p className="italic mt-2 text-sm ">What a pathetic service being provided. The tables were not clean and the flies were coming at us like the drones. And we were trying to save ourselves from these drone attacks in the meanwhile one of them succeeded to ruin our food at the table. It crashed into the food.</p>
+        <p className="italic mt-2 text-sm ">{selectedReview?.reviewmesg}</p>
             </CardContent>
         </Card>
         <h1 className="font-bold mt-4 text-sm">My Templates</h1>
