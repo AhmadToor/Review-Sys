343c07417e163a7d5bf92608b3406df48a67c237
Ahmad Toor <ahmadtoor4321@gmail.com>
2024-12-03 11:38:09 +0500

    Added state variables and conditional class names to toggle styles for plan selection in SettingsBillingTab and UpgradeProSection components.

--- src/components/settings/settingsBillingTab.tsx
+++ src/components/settings/settingsBillingTab.tsx
@@ -5,8 +5,11 @@ import GoogleIcon from '@/assets/svg/GoogleIcon.svg?react'
 import { useNavigate } from "react-router-dom";
 import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
 import AddCardComponent from "./addCardComponent";
+import { useState } from "react";
 
 const SettingsBillingTab = () => {
+    const [isfreetrial, setIsFreetrial] = useState(true)
+    const [isMonthlyPlan, setIsMonthlyPlan] = useState(true)
     const navigate = useNavigate()
   return (
     <div className="grid grid-cols-[4fr_5fr] gap-6">
@@ -16,11 +19,11 @@ const SettingsBillingTab = () => {
         <p className="text-gray-500 text-xs mt-1">Manage your current subscription plan here</p>
         <Card className="p-4 mt-4 border-none rounded-xl">
         <CardContent className="p-0 flex mb-4 flex-row gap-2">
-            <div className="border-[1px] w-full rounded-3xl p-3 px-4 bg-dashboard1 cursor-pointer  flex flex-col h-20 justify-end   border-gradient">
+            <div onClick={()=>{setIsFreetrial(true)}} className={`border-[1px] w-full rounded-3xl p-3 px-4 cursor-pointer  flex flex-col h-20 justify-end     ${isfreetrial && ("border-gradient bg-dashboard1 ")}`}>
                 <p className="text-[10px] text-gray-500">Current Plan</p>
                 <h1 className="font-semi-bold text-sm">30 Days Free Trial</h1>
             </div>
-            <div className="border-[1px] w-full  rounded-3xl p-3 px-4   cursor-pointer flex flex-col h-20 justify-end">
+            <div onClick={()=>{setIsFreetrial(false)}}  className={`border-[1px] w-full  rounded-3xl p-3 px-4   cursor-pointer flex flex-col h-20 justify-end ${!isfreetrial && ("border-gradient bg-dashboard2 ")} `}>
                 <p className="text-[10px] text-gray-500">Renews On</p>
                 <h1 className="font-semi-bold text-sm">24 Sept, 2024</h1>
             </div>
@@ -28,21 +31,21 @@ const SettingsBillingTab = () => {
         <CardFooter className="p-0">
             <Button variant='outline' className="w-full bg-white text-xs text-destructive">Cancel Subscription</Button>
         </CardFooter>
-        </Card>
+        </Card> 
         </div>
         <div >
         <h1 className="font-bold text-sm">See Available Plans</h1>
         <p className="text-gray-500 text-xs mt-1">Monthly Plan, Annual Plan per Business Profile</p>
         <Card className="lg:p-4 p-3 mt-4 border-none rounded-xl">
         <CardContent className="p-0 flex mb-4 flex-row gap-2">
-            <div className="border-[1px] w-full rounded-3xl p-3 lg:px-4 bg-white cursor-pointer border-gradient flex flex-col h-36 ">
+            <div onClick={()=>{setIsMonthlyPlan(true)}} className={`border-[1px] w-full rounded-3xl p-3 lg:px-4 bg-white cursor-pointer b flex flex-col h-36 ${isMonthlyPlan && ("border-gradient")} `}>
                 <div className="flex flex-col bg-dashboard1 rounded-2xl h-full items-center justify-center">
                     <h1 className="font-bold text-2xl">20$</h1>
                     <p className="text-gray-500 text-[8px] ">Per Buisness Profile</p>
                 </div>
                 <p className="mt-2 text-xs lg:text-sm text-center">Monthly Plan</p>
             </div>
-            <div className="border-[1px] w-full rounded-3xl p-3 px-4 bg-white cursor-pointer  flex flex-col h-36 ">
+            <div onClick={()=>{setIsMonthlyPlan(false)}} className={`border-[1px] w-full rounded-3xl p-3 px-4 bg-white cursor-pointer  flex flex-col h-36 ${!isMonthlyPlan && ("border-gradient")} `} >
                 <div className="flex flex-col bg-dashboard2 rounded-2xl h-full items-center justify-center">
                     <h1 className="font-bold text-2xl">240$</h1>
                     <p className="text-gray-500 text-[8px] ">Per Buisness Profile</p>
--- src/components/settings/upgradeProSection.tsx
+++ src/components/settings/upgradeProSection.tsx
@@ -5,8 +5,10 @@ import GoogleIcon from '@/assets/svg/GoogleIcon.svg?react'
 import { useNavigate } from "react-router-dom";
 import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
 import AddCardComponent from "./addCardComponent";
+import { useState } from "react";
 
 const UpgradeProSection = () => {
+    const [isMonthlyPlan, setIsMonthlyPlan] = useState(true)
     const navigate = useNavigate()
     return (
         <div className="grid grid-cols-[4fr_5fr] mt-6 gap-6">
@@ -17,20 +19,20 @@ const UpgradeProSection = () => {
                     <p className="text-gray-500 text-xs mt-1">Monthly Plan, Annual Plan per Business Profile</p>
                     <Card className="lg:p-4 p-3 mt-4 border-none rounded-xl">
                         <CardContent className="p-0 flex mb-4 flex-row gap-2">
-                            <div className="border-[1px] w-full rounded-3xl p-3 lg:px-4 bg-white cursor-pointer border-gradient flex flex-col h-36 ">
-                                <div className="flex flex-col bg-dashboard1 rounded-2xl h-full items-center justify-center">
-                                    <h1 className="font-bold text-2xl">20$</h1>
-                                    <p className="text-gray-500 text-[8px] ">Per Buisness Profile</p>
-                                </div>
-                                <p className="mt-2 text-xs lg:text-sm text-center">Monthly Plan</p>
-                            </div>
-                            <div className="border-[1px] w-full rounded-3xl p-3 px-4 bg-white cursor-pointer  flex flex-col h-36 ">
-                                <div className="flex flex-col bg-dashboard2 rounded-2xl h-full items-center justify-center">
-                                    <h1 className="font-bold text-2xl">240$</h1>
-                                    <p className="text-gray-500 text-[8px] ">Per Buisness Profile</p>
-                                </div>
-                                <p className="mt-2 text-xs lg:text-sm text-center">Yearly Plan </p>
-                            </div>
+                        <div onClick={()=>{setIsMonthlyPlan(true)}} className={`border-[1px] w-full rounded-3xl p-3 lg:px-4 bg-white cursor-pointer b flex flex-col h-36 ${isMonthlyPlan && ("border-gradient")} `}>
+                <div className="flex flex-col bg-dashboard1 rounded-2xl h-full items-center justify-center">
+                    <h1 className="font-bold text-2xl">20$</h1>
+                    <p className="text-gray-500 text-[8px] ">Per Buisness Profile</p>
+                </div>
+                <p className="mt-2 text-xs lg:text-sm text-center">Monthly Plan</p>
+            </div>
+            <div onClick={()=>{setIsMonthlyPlan(false)}} className={`border-[1px] w-full rounded-3xl p-3 px-4 bg-white cursor-pointer  flex flex-col h-36 ${!isMonthlyPlan && ("border-gradient")} `} >
+                <div className="flex flex-col bg-dashboard2 rounded-2xl h-full items-center justify-center">
+                    <h1 className="font-bold text-2xl">240$</h1>
+                    <p className="text-gray-500 text-[8px] ">Per Buisness Profile</p>
+                </div>
+                <p className="mt-2 text-xs lg:text-sm text-center">Yearly Plan </p>
+            </div>
 
                         </CardContent>
                         <CardFooter className="p-0">
@@ -66,7 +68,7 @@ const UpgradeProSection = () => {
             <p className="text-gray-500 text-xs mt-2">Have a look at your order and confirm to checkout.</p>
             <Card className="p-2 border-none mt-4 rounded-xl">
                         <CardContent className="p-3 flex flex-col gap-4">
-                            <Button variant="outline" className=" w-full justify-between  border-none text-xs bg-gray-100"><span>Total Today</span> <strong>20 USD</strong></Button>
+                            <Button variant="outline" className=" w-full justify-between  border-none text-xs bg-gray-100"><span>Total Today</span> <strong>{isMonthlyPlan? "20" : "240"} USD</strong></Button>
                            <div className="flex gap-2 mx-2">
                             <input type="checkbox" className="cursor-pointer" defaultChecked={true}/>
                             <p className="text-xs text-gray-500">After the 30-day trial period, you’ll be charged $20.00 USD/month plus applicable tax. Cancel any time.</p>
