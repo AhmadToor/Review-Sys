8920e2238986a25ce6850f320bd19f4c10330a44
Ahmad Toor <ahmadtoor4321@gmail.com>
2024-12-03 10:58:06 +0500

    Modified Feedback Data

--- src/components/feedback/feedbackComponent.tsx
+++ src/components/feedback/feedbackComponent.tsx
@@ -1,46 +1,22 @@
+import { feedback } from "@/data/feedbackTemplatesData";
 import { Card, CardContent, CardHeader } from "../ui/card";
+import { useState } from "react";
 
 
-const reviews = [
-    {
-        'title': "Review Our Business",
-        'description': "Hi [Customer Name], hope you are in good health. We are reaching out to see how we can improve this that bothers you and what can we do to help you better....",
-        'timestamp' : 'Sent 1 hour ago'
-    },
-    {
-        'title': "Requesting Review",
-        'description': "Hi [Customer Name], hope you are in good health. We are reaching out to see how we can improve this. We are reaching out to see how we can improve this....",
-        'timestamp' : 'Sent 1 hour ago'
-    },
-    {
-        'title': "Review Our Business",
-        'description': "Hi [Customer Name], hope you are in good health. We are reaching out to see how we can improve this. We are reaching out to see how we can improve this....",
-        'timestamp' : 'Sent 1 hour ago'
-    },
-    {
-        'title': "Review Our Business",
-        'description': "Hi [Customer Name], hope you are in good health. We are reaching out to see how we can improve this....",
-        'timestamp' : 'Sent 1 hour ago'
-    },
-    {
-        'title': "Review Our Business",
-        'description': "Hi [Customer Name], hope you are in good health. We are reaching out to see how we can improve this that bothers you and what can we do to help you better....",
-        'timestamp' : 'Sent 1 hour ago'
-    },
-]
 
 const FeedbackComponent = () => {
+    const [selectedTemplate, setSelectedTemplate] = useState(feedback[0])
   return (
     <div className="grid grid-cols-[3fr_2fr] gap-3 mt-3">
       <div className="space-y-3">
-       {reviews.map((review , index)=>{
+       {feedback.map((feedback , index)=>{
         return(
-            <Card key={index} className="p-3 border-none cursor-pointer">
+            <Card onClick={()=>{setSelectedTemplate(feedback)}} key={index} className="p-3 border-none cursor-pointer">
             <CardContent className="p-0">
-                <h1 className="text-sm font-semibold">{review.title}</h1>
-                <p className="mt-2 text-xs">{review.description}</p>
+                <h1 className="text-sm font-semibold">{feedback.title}</h1>
+                <p className="mt-2 text-xs">{`${feedback.description.slice(0,150)}...`}</p>
                 <hr className="my-2" />
-                <p className="text-[10px] text-gray-500 ">{review.timestamp}</p>
+                <p className="text-[10px] text-gray-500 ">{feedback.timestamp}</p>
             </CardContent>
             </Card>
         )
@@ -57,24 +33,14 @@ const FeedbackComponent = () => {
 
                     <hr />
 
-                    <div className="flex gap-2 text-xs "><span className="text-gray-500">Subject:</span> <p className="font-semibold">Review Our Buisness</p></div>
+                    <div className="flex gap-2 text-xs "><span className="text-gray-500">Subject:</span> <p className="font-semibold">{selectedTemplate.title}</p></div>
                     <hr />
 
                 </CardHeader>
                 <CardContent className=" text-sm">
 
                     <p className="text-xs text-gray-500 mb-3 ">Body:</p>
-                    <p>Hi [Customer Name], hope you are in good health.</p>
-                    <br />
-                    <p>We have an exciting update for you: there are now no fewer than 3 brand new places ready to rent in Lahore.</p>
-                    <br />
-                    <p>Are you curious about the available rental options in Lahore?</p>
-                    <br />
-                    <p>Interesting options have recently been added that seamlessly meet your wishes for your new stay. Do you see something that appeals to you? You have the option to arrange a viewing directly via the website or ask questions about the place in question. It’s now easier than ever to take your next step towards your ideal home.</p>
-                    <br />
-                    <p>Maan jao na yaaaar. Apni ki hui bakwas wapis lylo.</p>
-                    <br />
-                    <p>Thanks</p>
+                    <div dangerouslySetInnerHTML={{ __html: selectedTemplate.description.replace(/(?:\r\n|\r|\n)/g, '<br />') }}/>
 
 
                 </CardContent>
---
+++ src/data/feedbackTemplatesData.tsx
@@ -0,0 +1,27 @@
+export const feedback = [
+    {
+        'title': "Customer Experience Survey",
+        'description': "Dear [Customer Name],\n\nWe hope this message finds you well. We are eager to hear your thoughts on your recent experience with us. Your feedback is crucial for us to identify areas of improvement.\n\nPlease share any concerns or suggestions you may have. Thank you for helping us serve you better.",
+        'timestamp' : 'Sent 2 hours ago'
+    },
+    {
+        'title': "We Value Your Opinion",
+        'description': "Hello [Customer Name],\n\nWe trust you are doing great. As part of our commitment to excellence, we would love to understand your experience with our services. Your insights will guide us in enhancing our offerings.\n\nFeel free to express any feedback you may have. We appreciate your time!",
+        'timestamp' : 'Sent 2 hours ago'
+    },
+    {
+        'title': "Help Us Improve",
+        'description': "Hi [Customer Name],\n\nWe hope you are well. Your opinion matters to us, and we are reaching out to gather your thoughts on our services. Identifying what works and what doesn’t is key to our growth.\n\nPlease let us know how we can serve you better. Thank you!",
+        'timestamp' : 'Sent 2 hours ago'
+    },
+    {
+        'title': "Your Feedback is Important",
+        'description': "Dear [Customer Name],\n\nGreetings! We are committed to providing you with the best service possible. To achieve this, we need your valuable feedback on your recent experience with us.\n\nWe look forward to hearing your thoughts and suggestions. Thank you for your support!",
+        'timestamp' : 'Sent 2 hours ago'
+    },
+    {
+        'title': "Share Your Thoughts with Us",
+        'description': "Hello [Customer Name],\n\nWe hope you are having a wonderful day. We are reaching out to gather your feedback on our services. Your insights are vital for us to understand how we can improve and better meet your needs.\n\nThank you for taking the time to share your thoughts with us!",
+        'timestamp' : 'Sent 2 hours ago'
+    },
+]
